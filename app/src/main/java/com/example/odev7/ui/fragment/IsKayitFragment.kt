b8b4094c52ce2eb646a06e5fbdb8615203d5d8c2
Mürsel <murselcetin51@gmail.com>
2022-07-06 16:49:09 +0300

    Anasayfa Fragment Oluşturuldu. Anasayfa Fragment Tasarımı düzenlendi. İş Kayıt Fragment Oluşturuldu. İş Kayıt  Fragment Tasarımı düzenlendi. İş Detay Fragment Oluşturuldu. İş Detay Fragment Tasarımı düzenlendi. Kullanılacak İconlar Eklendi.

---
+++ app/src/main/java/com/example/odev7/ui/fragment/IsKayitFragment.kt
@@ -0,0 +1,21 @@
+package com.example.odev7.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.odev7.R
+
+
+class IsKayitFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_is_kayit, container, false)
+    }
+
+}
